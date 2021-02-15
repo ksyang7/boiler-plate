a72c42aa9cd7d191d5ae0be5b13bd2758333d4e5
yangseung-eun <ksook12@gmail.com>
2021-02-15 18:53:13 +0900

    Reducer를 이용한 회원가입페이지

--- client/src/_reducer/index.js
+++ client/src/_reducer/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-//import user from './user_reducer';
+import user from './user_reducer';
 //import comment from './comment_reducer';
 
-const rootReducer = combineReducer({
-    //user,
+const rootReducer = combineReducers({
+    user
     //comment
 })
 
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
 function LoginPage() {
+    
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) =>{
+        setEmail(event.currentTarget.value);
+    }
+
+    const onPasswordHandler = (event) =>{
+        setPassword(event.currentTarget.value); 
+    }
+
+    const onSubmitHandler = (event) =>{
+        event.preventDefault(); // 화면 리프레시 방지 
+
+        let body ={
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if(response.payload.loginSuccess){
+                    this.props.history.push('/')
+                } else {
+                    alert('Error')
+                }
+            })
+        }
+
     return (
-        <div>
-           LoginPage 
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <from style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={ Email } onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={ Password } onChange={onPasswordHandler} />
+                <br />
+                <button type="submit" >
+                    Login
+                </button>
+            </from>
         </div>
     )
 }
